e9ac7a0975f80a3eb422e6ded863607aef122b18
ValentynK <dgonvk@gmail.com>
2022-07-12 21:00:33 +0300

    Create a fixed follow camera

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,16 +5,6 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [field: SerializeField]
-    public Transform Target { get; set; }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
