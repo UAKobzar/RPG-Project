ad5c52b4da63bce30c0f2bc564feabffabf42ff3
ValentynK <dgonvk@gmail.com>
2022-07-13 23:26:21 +0300

    Raycasting for components

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    internal class Fighter : MonoBehaviour
+    {
+        public void Atack(CombatTarget target)
+        {
+            print("Take that you short, squat peasant!");
+        }
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using RPG.Movement;
+﻿using RPG.Combat;
+using RPG.Movement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,40 @@ namespace RPG.Control
     {
         public void Update()
         {
+            InteractWithMovement();
+            InteractWithCombat();
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(1))
             {
                 MoveToCursor();
             }
         }
 
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits)
+            {
+                var target = hit.transform.gameObject.GetComponent<CombatTarget>();
+
+                if (target != null)
+                {
+                    if (Input.GetMouseButton(1))
+                    {
+                        Fighter fighterComponent = GetComponent<Fighter>();
+                        fighterComponent?.Atack(target);
+                    }
+                }
+            }
+        }
+
         private void MoveToCursor()
         {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            bool isHit = Physics.Raycast(ray, out RaycastHit hitInfo);
+            bool isHit = Physics.Raycast(GetMouseRay(), out RaycastHit hitInfo);
 
             if (isHit)
             {
@@ -29,6 +54,11 @@ namespace RPG.Control
 
                 component.MoveTo(hitInfo.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
